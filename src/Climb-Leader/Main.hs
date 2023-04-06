9eb2331983f78dad1aa13f585fe4cbbcc95ca677
lim6112j <lim6112j@gmail.com>
2023-04-07 02:16:13 +0900

    climb-leader problem

---
+++ src/Climb-Leader/Main.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE DuplicateRecordFields, FlexibleInstances, UndecidableInstances #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Data.Text
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+--
+-- Complete the 'climbingLeaderboard' function below.
+--
+-- The function is expected to return an INTEGER_ARRAY.
+-- The function accepts following parameters:
+--  1. INTEGER_ARRAY ranked
+--  2. INTEGER_ARRAY player
+--
+
+climbingLeaderboard ranked player = do
+    -- Write your code here
+
+lstrip = Data.Text.unpack . Data.Text.stripStart . Data.Text.pack
+rstrip = Data.Text.unpack . Data.Text.stripEnd . Data.Text.pack
+
+main :: IO()
+main = do
+    stdout <- getEnv "OUTPUT_PATH"
+    fptr <- openFile stdout WriteMode
+
+    rankedCountTemp <- getLine
+    let rankedCount = read $ lstrip $ rstrip rankedCountTemp :: Int
+
+    rankedTemp <- getLine
+
+    let ranked = Data.List.map (read :: String -> Int) . Data.List.words $ rstrip rankedTemp
+
+    playerCountTemp <- getLine
+    let playerCount = read $ lstrip $ rstrip playerCountTemp :: Int
+
+    playerTemp <- getLine
+
+    let player = Data.List.map (read :: String -> Int) . Data.List.words $ rstrip playerTemp
+
+    let result = climbingLeaderboard ranked player
+
+    hPutStrLn fptr $ Data.List.intercalate "\n" $ Data.List.map (\x -> show x) $ result
+
+    hFlush fptr
+    hClose fptr
